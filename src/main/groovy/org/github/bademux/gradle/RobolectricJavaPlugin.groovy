a5d595de42cc20395664483c3d936d24b5e1160a
Bademus <bademux+git@gmail.com>
2014-12-01 01:49:12 +0100

    clenup impl\fixes
    check the test project https://github.com/bademux/deckard-gradle

--- src/main/groovy/org/github/bademux/gradle/RobolectricJavaPlugin.groovy
+++ src/main/groovy/org/github/bademux/gradle/RobolectricJavaPlugin.groovy
@@ -1,44 +1,51 @@
 package org.github.bademux.gradle
 
-import com.android.build.gradle.tasks.ProcessAndroidResources
 import com.android.builder.core.DefaultManifestParser
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.testing.Test
 
 class RobolectricJavaPlugin implements Plugin<Project> {
 
-    private Project project
-
-    void apply(final Project project) {
-        this.project = project
-        def extension = project.extensions.create('robolectric', RobolectricJavaPluginExtension)
-        project.evaluationDependsOn(extension.androidProjectName)
-        final String buildType = extension.buildType
-        final String flavorName = extension.flavorName
-
+    void apply(Project project) {
         project.plugins.apply JavaPlugin
+        project.extensions.create('robolectric', RobolectricJavaPluginExtension)
 
-        project.project(extension.androidProjectName) {
-            if (!getProject().hasProperty('android')) {
-                throw new IllegalArgumentException("'$name' is invalid android project")
-            }
-            def variant = android.applicationVariants.find {
-                it.variantData.capitalizedBuildTypeName.equals(buildType) &&
-                it.variantData.capitalizedFlavorName.equals(flavorName)
-            }
-            if (variant == null) {
-                throw new IllegalArgumentException(
-                        "Unknown flavorName:${flavorName} or buildType:${buildType}")
-            }
+        project.afterEvaluate { p -> process(p) }
 
-            configureProject(variant, getPackageName(android))
+    }
+
+    private static void process(final Project project) {
+        def ext = project.extensions.robolectric
+        project.evaluationDependsOn(ext.androidProjectName)
+
+        project.project(ext.androidProjectName) { aproject ->
+            configureProject(project,
+                             check(aproject, ext.flavorName, ext.buildType),
+                             getPackageName(aproject.android))
         }
     }
 
-    private void configureProject(variant, String packageName) {
-        ProcessAndroidResources processResourcesTask = variant.outputs.get(0).processResources
+    private static def check(final Project aproject,
+                             final String flavorName, final String buildType) {
+        if (!aproject.hasProperty('android')) {
+            throw new IllegalArgumentException("'${aproject.name}' is invalid android project")
+        }
+        def variant = aproject.android.applicationVariants.find {
+            it.variantData.capitalizedBuildTypeName.equals(buildType) &&
+            it.variantData.capitalizedFlavorName.equals(flavorName)
+        }
+        if (variant == null) {
+            throw new IllegalArgumentException(
+                    "Unknown flavorName:${flavorName} or buildType:${buildType}")
+        }
+        return variant
+    }
+
+    private static void configureProject(final Project project, variant, String packageName) {
+        Task processResourcesTask = variant.outputs.get(0).processResources
         //add dependency
         project.tasks.processTestResources.dependsOn processResourcesTask
         project.tasks.compileTestJava.dependsOn variant.javaCompile
@@ -56,7 +63,7 @@ class RobolectricJavaPlugin implements Plugin<Project> {
 
     }
 
-    private String getPackageName(android) {
+    private static String getPackageName(android) {
         String packageName = android.defaultConfig.applicationId
         if (packageName != null && !packageName.isEmpty()) {
             return packageName
